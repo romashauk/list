4b6988a35b2a220501c014c3a56662c03d81c8aa
roma.shauk <roman.shauk@dataprobsoton.com>
2019-08-04 17:21:35 +0300

    Updates

---
+++ precache-manifest.a7fccc17d5880b4295a58cc17f50d4e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f85dbe54ce5e80dbd9d4897285613ab3",
+    "url": "/list/index.html"
+  },
+  {
+    "revision": "55381802f2e05436658b",
+    "url": "/list/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "9f3832436db0395c912a",
+    "url": "/list/static/css/main.50426b00.chunk.css"
+  },
+  {
+    "revision": "55381802f2e05436658b",
+    "url": "/list/static/js/2.8fd2f2c5.chunk.js"
+  },
+  {
+    "revision": "9f3832436db0395c912a",
+    "url": "/list/static/js/main.cabda545.chunk.js"
+  },
+  {
+    "revision": "893017e7fd6b6b846040",
+    "url": "/list/static/js/runtime~main.7fc17663.js"
+  }
+]);
